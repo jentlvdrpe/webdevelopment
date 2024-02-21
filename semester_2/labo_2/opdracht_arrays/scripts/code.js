c537c1b9bd952fca5ccea178154a0f859e2514c8
jentlvdrpe <jentl.vandorpe@student.vives.be>
2024-02-21 18:30:21 +0100

    Begonnen Opdracht Arrays

---
+++ semester_2/labo_2/opdracht_arrays/scripts/code.js
@@ -0,0 +1,8 @@
+const setup = () => {
+    let familieNamen = ['Naam1','Naam2','Naam3','Naam4','Naam5']
+    console.log(familieNamen.length)
+    console.log(familieNamen[0])
+    console.log(familieNamen[2])
+    console.log(familieNamen[4])
+}
+window.addEventListener("load", setup);
