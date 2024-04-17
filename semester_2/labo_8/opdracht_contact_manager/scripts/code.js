4e38d003b2fee994a5f5ec8cdccb4a961419249b
jentlvdrpe <jentl.vandorpe@student.vives.be>
2024-04-17 14:39:41 +0200

    Labo 8 - Contact Manager

--- semester_2/labo_8/opdracht_contact_manager/scripts/code.js
+++ semester_2/labo_8/opdracht_contact_manager/scripts/code.js
@@ -1,3 +1,53 @@
+let personen = [];
+
+const bewaarBewerktePersoon = () => {
+    valideer();
+    if(document.querySelectorAll('.invalid').length > 0) return;
+
+    let select = document.querySelector('#lstPersonen');
+    let persoon = {
+        voornaam: document.querySelector('#txtVoornaam').value,
+        familienaam: document.querySelector('#txtFamilienaam').value,
+        geboorteDatum: document.querySelector('#txtGeboorteDatum').value,
+        email: document.querySelector('#txtEmail').value,
+        kinderen: document.querySelector('#txtAantalKinderen').value
+    };
+
+    let index = select.selectedIndex;
+    if(index < 0){
+        personen.push(persoon);
+        index = personen.length - 1;
+        select.innerHTML += `<option id="${index}">${personen[index].voornaam} ${personen[index].familienaam}</option>`
+    } else {
+        personen[index] = persoon;
+        select.options[index].textContent = `${personen[index].voornaam} ${personen[index].familienaam}`;
+    }
+};
+
+const bewerkNieuwePersoon = () => {
+    document.querySelector('#lstPersonen').selectedIndex = -1;
+    let texts = document.querySelectorAll('input[type="text"]');
+    texts.forEach(text => { text.value = ""; })
+};
+
+const zetWaarden = (event) => {
+    let id = event.currentTarget.selectedIndex;
+    document.querySelector('#txtVoornaam').value = personen[id].voornaam;
+    document.querySelector('#txtFamilienaam').value = personen[id].familienaam;
+    document.querySelector('#txtGeboorteDatum').value = personen[id].geboorteDatum;
+    document.querySelector('#txtEmail').value = personen[id].email;
+    document.querySelector('#txtAantalKinderen').value = personen[id].kinderen;
+};
+
 const setup = () => {
-}
+    let btnBewaar = document.getElementById("btnBewaar");
+    btnBewaar.addEventListener("click", bewaarBewerktePersoon);
+
+    let btnNieuw = document.getElementById("btnNieuw");
+    btnNieuw.addEventListener("click", bewerkNieuwePersoon);
+
+    let lstPersonen = document.getElementById("lstPersonen");
+    lstPersonen.addEventListener('change', zetWaarden);
+};
+
 window.addEventListener("load", setup);
